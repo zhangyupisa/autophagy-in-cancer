50bc64d08cf427a2cc38502ab80a8f41752e8d0f
chunjie-sam-liu <chunjie-sam-liu@foxmail.com>
2018-07-13 18:45:21 +0800

    latex2exp for the human read science notation

--- 21-autophagy-flux-measure/01-autopahgy-flux.R
+++ 21-autophagy-flux-measure/01-autopahgy-flux.R
@@ -25,7 +25,7 @@ rppa_name <- readr::read_rds(path = file.path(path_data, "rppa-name-symbol.rds.g
 gene_list <- readr::read_rds(path = file.path(path_data, "rds-03-a-atg-lys-gene-list.rds.gz"))
 
 # pancan atlas cancer tissue color
-pcc <- readr::read_tsv(file = file.path(path_data, "PanCanAtlasTumors_color_coded_by_organ_system_20170302.tsv"))
+pcc <- readr::read_tsv(file = file.path(path_data, "PanCanAtlasTumors-color-coded-by-organ-system-20170302.tsv"))
 
 # rppa pathway score
 pathway_score <- readr::read_rds(path = file.path(path_data, "pancan32-rppa-score.rds.gz"))
@@ -86,7 +86,7 @@ p62_rppa_expr %>%
     coef = purrr::map(
       .x = p62_sqstm1,
       .f = function(.x) {
-        cor.test(.x$p62, .x$sqstm1, method = "pearson") %>% 
+        cor.test(.x$p62, log2(.x$sqstm1 + 1), method = "spearman", exact = FALSE) %>% 
           broom::tidy() %>% 
           dplyr::select(coef = estimate, pval = p.value)
       }
@@ -95,7 +95,71 @@ p62_rppa_expr %>%
   tidyr::unnest(coef) ->
   p62_sqstm1_coef
 
-p62_sqstm1_coef %>% dplyr::arrange(coef) %>% print(n = Inf)
+human_read <- function(.x){
+  if (.x > 0.1) {
+    .x %>% round(digits = 2) %>% toString()
+  } else if (.x < 0.1 && .x > 0.001 ) {
+    .x %>% signif(digits = 1) %>% toString()
+  } else {
+    .x %>% signif(digits = 2) %>% toString()
+  }
+}
+
+p62_sqstm1_coef %>% 
+  dplyr::select(1, 2) %>% 
+  tidyr::unnest() %>% 
+  dplyr::mutate(sqstm1 = log2(sqstm1 + 1)) -> 
+  p62_sqstm1_expr
+
+p62_sqstm1_coef %>% 
+  dplyr::select(-2) %>% 
+  dplyr::inner_join(pcc, by = "cancer_types") %>% 
+  dplyr::mutate(label = glue::glue("{cancer_types} R={format(coef, digits = 2)}")) %>% 
+  dplyr::select(1, 2, 3, 4, 6) %>% 
+  dplyr::mutate(pval = purrr::map_chr(pval, human_read)) %>% 
+  dplyr::mutate(
+    label = purrr::map2(
+      .x = pval,
+      .y = label,
+      .f = function(.x, .y) {
+        if (grepl(pattern = "e", x = .x)) {
+          sub("-0", "-", strsplit(split = "e", x = .x, fixed = TRUE)[[1]]) -> .xx
+          latex2exp::TeX(glue::glue("<<.y>> p=$<<.xx[1]>> \\times 10^{<<.xx[2]>>}$", .open = "<<", .close = ">>"))
+        } else {
+          latex2exp::TeX(glue::glue("{.y} p={.x}"))
+        }
+      }
+    )
+  ) %>% 
+  dplyr::arrange(dplyr::desc(coef)) ->
+  p62_sqstm1_coef_label
+
+
+p62_sqstm1_expr %>% 
+  dplyr::mutate(sqstm1 = ifelse(sqstm1 > 18.6, 18.6, sqstm1)) %>%
+  dplyr::mutate(sqstm1 = ifelse(sqstm1 < 9, 9, sqstm1)) %>%
+  dplyr::mutate(p62 = ifelse(p62 < -2, -2, p62)) %>%
+  dplyr::mutate(p62 = ifelse(p62 > 7.5, 7.5, p62)) %>%
+  ggplot(aes(y = p62, x = sqstm1, color = cancer_types)) +
+  geom_point() +
+  scale_color_manual(
+    values = p62_sqstm1_coef_label$color, 
+    limits = p62_sqstm1_coef_label$cancer_types,
+    labels = p62_sqstm1_coef_label$label,
+    name = ""
+  ) +
+  theme(
+    panel.background = element_rect(fill = "white", colour = "black")
+  ) +
+  labs(
+    x = "mRNA expression (log2)",
+    y = "Protein expression",
+    title = "p62 protein vs. mRNA"
+  )
+
+
+#
+#
 
 # p62 correlates with pathway score ---------------------------------------
 # p62 rppa expression correlates with the rppa score.
