1a73ce46dabb8f163982de7031bd56d9c419c237
chunjie-sam-liu <chunjie-sam-liu@foxmail.com>
2018-09-07 13:19:38 +0800

    autophagy gsva

--- 21-autophagy-flux-measure/03-tcga-autophagy-gsva-score.R
+++ 21-autophagy-flux-measure/03-tcga-autophagy-gsva-score.R
@@ -11,11 +11,20 @@ path_data <- file.path(path_project, "TCGA")
 path_out <- file.path(path_project, "21-autophagy-flux")
 
 
+
 # load data ---------------------------------------------------------------
+tcga_gene_symbol <- readr::read_rds(file.path(path_data, 'tcga-gene-symbol.rds.gz'))
+
+
 # pancan33-expr.rds.gz
 mrna <- readr::read_rds(path = file.path(path_data, "pancan33-expr.rds.gz"))
 
 gene_list <- readr::read_rds(path = file.path(path_data, "rds-03-a-atg-lys-gene-list.rds.gz"))
+
+gene_list %>% 
+  dplyr::select(symbol) %>% 
+  dplyr::left_join(tcga_gene_symbol, by = 'symbol') %>% 
+  print(n = Inf)
 
 gene_sets <- list(
   atg_lys = dplyr::filter(gene_list, type %in% c("atg", "lys")),
@@ -26,8 +35,8 @@ gene_sets <- list(
   lys_com = dplyr::filter(gene_list, pathway == "lysosome composition"),
   lys_deg = dplyr::filter(gene_list, pathway == "lysosome degradation")
 ) %>% purrr::map("symbol")
-#
-# 
+
+
 
 
 
@@ -35,61 +44,79 @@ gene_sets <- list(
 
 library(GSVA)
 
-fn_gsva <- function(.x, .y, gene_sets = gene_sets){
-  # .x <- .te$cancer_types
-  # .y <- .te$filter_expr[[1]]
+fn_gsva <- function(.x, .y, gene_sets = gene_sets) {
+  .x <- .te$cancer_types
+  .y <- .te$expr[[1]]
   print(.x)
-  
+
+  # extract tumor
   names(.y)[c(-1, -2)] -> .b
-  .b[substr(.b, start = 14, stop = 14) == 0] -> .b
-  
-  .y %>% 
-    tidyr::drop_na() %>% 
-    dplyr::select(1, .b) %>% 
-    dplyr::filter_at(.vars = dplyr::vars(dplyr::starts_with("T")), .vars_predicate = dplyr::any_vars(. != 0)) -> 
+  .b[substr(.b, start = 14, stop = 15) == "01"] -> .b
+
+  .y %>%
+    dplyr::select(1, .b) %>%
+    tidyr::drop_na() %>%
+    dplyr::filter_at(
+      .vars = dplyr::vars(dplyr::starts_with("T")), 
+      .vars_predicate = dplyr::any_vars(. != 0)
+    ) ->
     .d
-  
-  .d_mat <- as.matrix(.d[,-1])
+
+  .d_mat <- as.matrix(.d[, -1])
   rownames(.d_mat) <- .d$symbol
-  
-  .es_dif <- gsva(.d_mat, gene_sets, method = "gsva", mx.diff = TRUE, verbose = FALSE, parallel.sz = 1)
-  
-  .es_dif %>% 
-    as.data.frame() %>% 
-    tibble::as_tibble() %>% 
+
+  .es_dif <- gsva(
+    expr = .d_mat, 
+    gset.idx.list = gene_sets,
+    rnaseq = TRUE,
+    method = "gsva",
+    mx.diff = TRUE, 
+    verbose = FALSE, 
+    parallel.sz = 1
+  )
+
+  .es_dif %>%
+    as.data.frame() %>%
+    tibble::as_tibble() %>%
     tibble::add_column(set = rownames(.es_dif), .before = 1)
 }
 
 
-# mrna %>% 
-#   dplyr::mutate(gsva = purrr::map2(.x = cancer_types, .y = expr, .f = fn_gsva, gene_sets = gene_sets)) %>% 
-#   dplyr::select(-expr) -> 
+# mrna %>%
+#   dplyr::mutate(gsva = purrr::map2(.x = cancer_types, .y = expr, .f = fn_gsva, gene_sets = gene_sets)) %>%
+#   dplyr::select(-expr) ->
 #   gene_set_gsva
 
 
 cl <- multidplyr::create_cluster(nrow(mrna))
-mrna %>% 
+mrna %>%
   multidplyr::partition(cluster = cl) %>%
   multidplyr::cluster_library("magrittr") %>%
   multidplyr::cluster_library("GSVA") %>%
   multidplyr::cluster_assign_value("gene_sets", gene_sets) %>%
   multidplyr::cluster_assign_value("fn_gsva", fn_gsva) %>%
-  dplyr::mutate(gsva = purrr::map2(.x = cancer_types, .y = expr, .f = fn_gsva, gene_sets = gene_sets)) %>%
-  dplyr::select(-expr) %>% 
+  dplyr::mutate(
+    gsva = purrr::map2(
+      .x = cancer_types, 
+      .y = expr, 
+      .f = fn_gsva, 
+      gene_sets = gene_sets
+      )
+    ) %>%
+  dplyr::select(-expr) %>%
   dplyr::collect() %>%
   dplyr::ungroup() %>%
   dplyr::select(-PARTITION_ID) ->
-  gene_set_gsva
-  
+gene_set_gsva
+
 parallel::stopCluster(cl)
 
-if (!file.exists(file.path(path_data, "tcga-autophagy-gsva.rds.gz"))) 
+if (!file.exists(file.path(path_data, "tcga-autophagy-gsva.rds.gz"))) {
   readr::write_rds(x = gene_set_gsva, path = file.path(path_data, "tcga-autophagy-gsva.rds.gz"), compress = "gz")
+}
 
 
 
 
 
 # End ---------------------------------------------------------------------
-
-
