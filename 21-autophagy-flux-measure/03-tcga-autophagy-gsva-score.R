b53265641b7dc9522de00b78ab7ace125ccb9d03
chunjie-sam-liu <chunjie-sam-liu@foxmail.com>
2018-09-12 12:16:36 +0800

    remodel the gsva

--- 21-autophagy-flux-measure/03-tcga-autophagy-gsva-score.R
+++ 21-autophagy-flux-measure/03-tcga-autophagy-gsva-score.R
@@ -16,15 +16,18 @@ path_out <- file.path(path_project, "21-autophagy-flux")
 tcga_gene_symbol <- readr::read_rds(file.path(path_data, 'tcga-gene-symbol.rds.gz'))
 
 
+
+
 # pancan33-expr.rds.gz
 mrna <- readr::read_rds(path = file.path(path_data, "pancan33-expr.rds.gz"))
 
-gene_list <- readr::read_rds(path = file.path(path_data, "rds-03-a-atg-lys-gene-list.rds.gz"))
+gene_list <- readr::read_rds(path = file.path(path_data, "rds-03-a-atg-lys-gene-list.rds.gz")) 
+
 
 gene_list %>% 
   dplyr::select(symbol) %>% 
-  dplyr::left_join(tcga_gene_symbol, by = 'symbol') %>% 
-  print(n = Inf)
+  dplyr::left_join(tcga_gene_symbol, by = 'symbol') ->
+  gene_list_entrez
 
 gene_sets <- list(
   atg_lys = dplyr::filter(gene_list, type %in% c("atg", "lys")),
@@ -34,9 +37,19 @@ gene_sets <- list(
   atg_form = dplyr::filter(gene_list, pathway == "autophagesome formation"),
   lys_com = dplyr::filter(gene_list, pathway == "lysosome composition"),
   lys_deg = dplyr::filter(gene_list, pathway == "lysosome degradation")
-) %>% purrr::map("symbol")
-
-
+) %>% 
+  purrr::map("symbol") %>% 
+  purrr::map(
+    .f = function(.x){
+      gene_list_entrez %>% 
+        dplyr::filter(symbol %in% .x) %>% 
+        dplyr::pull(entrez_id) ->
+        .v
+      names(.v) <- .x
+      .v
+    }
+  )
+  
 
 
 
@@ -45,30 +58,41 @@ gene_sets <- list(
 library(GSVA)
 
 fn_gsva <- function(.x, .y, gene_sets = gene_sets) {
-  .x <- .te$cancer_types
-  .y <- .te$expr[[1]]
-  print(.x)
+  # .x <- .te$cancer_types
+  # .y <- .te$expr[[1]]
+  # print(.x)
 
   # extract tumor
   names(.y)[c(-1, -2)] -> .b
   .b[substr(.b, start = 14, stop = 15) == "01"] -> .b
 
   .y %>%
-    dplyr::select(1, .b) %>%
+    dplyr::select(2, .b) %>%
+    dplyr::mutate(entrez_id = as.integer(entrez_id)) %>% 
     tidyr::drop_na() %>%
     dplyr::filter_at(
       .vars = dplyr::vars(dplyr::starts_with("T")), 
       .vars_predicate = dplyr::any_vars(. != 0)
+    ) %>% 
+    dplyr::mutate_at(
+      .vars = dplyr::vars(dplyr::starts_with("T")),
+      .funs = dplyr::funs(log2(. + 1))
     ) ->
     .d
 
   .d_mat <- as.matrix(.d[, -1])
-  rownames(.d_mat) <- .d$symbol
+  rownames(.d_mat) <- .d$entrez_id
 
+  # the signature is 'matrix, list'
+  # showMethods(f = 'gsva')
+  # getMethod(f = 'gsva', signature = list(expr = 'matrix', gset.idx.list = 'list'))
+  # for the gsva 'kcdf' the GAUSSION for microarray,
+  # 'Poisson' for the rnaseq count,
+  
   .es_dif <- gsva(
     expr = .d_mat, 
     gset.idx.list = gene_sets,
-    rnaseq = TRUE,
+    kcdf = "Gaussian",
     method = "gsva",
     mx.diff = TRUE, 
     verbose = FALSE, 
